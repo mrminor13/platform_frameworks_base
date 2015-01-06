4091387d23c47dc1651f47f6f836a04ff7b23f5d
Daxxmax <>
2015-01-06 08:47:44 +0100

    FWB: Statusbar clock font style (1/2)
    AICPfy
    Bring back to L
    
    Change-Id: Iafb811b9f819a100940fdeeaa2c657483c039950

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4208,6 +4208,12 @@ public final class Settings {
         public static final String STATUS_BAR_WEATHER_COLOR = "status_bar_weather_color";
 
         /**
+         * Settings for clock font style
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_FONT_STYLE = "statusbar_clock_font_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
