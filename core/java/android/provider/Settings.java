c5320276adf242dd16466e6e8da54e5604642536
Jon Haus <haus.xda@gmail.com>
2015-03-23 20:13:35 +0100

    FWB: Network traffic meter (1/2)
    Change-Id: I00f096784a238b3828c4e0a79ecfe410b69bc888

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3618,6 +3618,16 @@ public final class Settings {
             PUBLIC_SETTINGS.add(SHOW_WEB_SUGGESTIONS);
         }
 
+       /**
+         * Network traffic indicator, goes from least to greatest significant bitwise
+         * 0 = Display up-stream traffic if set
+         * 1 = Display down-stream traffic if set
+         * 2 = Show as Byte/s if set
+         * 16-31 = Refresh interval(ms) min: 250 max: 32750 default: 1000
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
         /**
          * These are all hidden system settings.
          *
