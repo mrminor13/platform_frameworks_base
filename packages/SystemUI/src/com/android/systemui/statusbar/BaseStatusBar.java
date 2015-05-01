da15d8db4b28f7fc8488c3f2605b8b34444ee8d1
aow1980 <wilkes.allen@gmail.com>
2015-04-30 22:51:27 -0400

    Fix build

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -2166,9 +2166,6 @@ public abstract class BaseStatusBar extends SystemUI implements
                 mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
 
         boolean isIMEShowing = inputMethodManager.isImeShowing();
-        final KeyguardTouchDelegate keyguard = KeyguardTouchDelegate.getInstance(mContext);
-        boolean keyguardIsShowing = keyguard.isShowingAndNotOccluded()
-                && keyguard.isInputRestricted();
 
         boolean isExpanded = false;
             if (mStackScroller != null) {
@@ -2180,9 +2177,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                 && !accessibilityForcesLaunch
                 && mPowerManager.isScreenOn()
     	        && !isIMEShowing
-                && !keyguardIsShowing
-                && !isExpanded
-                && !zenBlocksHeadsUp;
+                && !isExpanded;
 
         try {
             interrupt = interrupt && !mDreamManager.isDreaming();
@@ -2192,7 +2187,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
         // its below our threshold priority, we might want to always display
         // notifications from certain apps
-        if (!isHighPriority && !isOngoing && !keyguardIsShowing && !isExpanded && !zenBlocksHeadsUp && !isIMEShowing) {
+        if (!isHighPriority && !isOngoing && !isExpanded && !isIMEShowing) {
             // However, we don't want to interrupt if we're in an application that is
             // in Do Not Disturb
             if (!isPackageInDnd(getTopLevelPackage())) {
