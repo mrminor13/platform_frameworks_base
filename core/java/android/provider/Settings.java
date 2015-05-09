c2757def45ce792b1b3c2200f56b0671110621b2
aow1980 <wilkes.allen@gmail.com>
2015-05-09 16:11:39 -0400

    [1/3] Frameworks: Proximity speaker
    Patchset: add time delay preference
    Change-Id: I8846e1d54b7f26f4322b91fc1ba20f3e5da465ec
    
    (cherry picked from commit 4b2247692b9590f5890a169bf5bd36b76a142969)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2259,6 +2259,27 @@ public final class Settings {
          */
         public static final String APPEND_FOR_LAST_AUDIBLE = "_last_audible";
 
+
+        /**
+         * Whether the proximity sensor will adjust call to speaker
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER = "proximity_auto_speaker";
+
+        /**
+         * Time delay to activate speaker after proximity sensor triggered
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER_DELAY = "proximity_auto_speaker_delay";
+
+        /**
+         * Whether the proximity sensor will adjust call to speaker,
+         * only while in call (not while ringing on outgoing call)
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER_INCALL_ONLY =
+                "proximity_auto_speaker_incall_only";
+
         /**
          * Persistent store for the system-wide default ringtone URI.
          * <p>
