8f71be0df9c8095dd7815a8bb735b4eaa29435f5
aow1980 <wilkes.allen@gmail.com>
2015-05-10 19:15:47 -0400

    QS: add Sync tile (1/2)
    Change-Id: I0c0bb51e736a01661566f1118f955e32eba96063
    Signed-off-by: RobbieL811 <RobbieLancaster811@gmail.com>
    
    (cherry picked from commit ed904595926a92560058f09f4d37e239f5efcb96)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -50,6 +50,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NotificationsTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.WifiTile;
 import com.android.systemui.settings.CurrentUserTracker;
 import com.android.systemui.statusbar.policy.BluetoothController;
@@ -305,6 +306,8 @@ public class QSTileHost implements QSTile.Host {
                 return new BatterySaverTile(this);
             case QSConstants.TILE_EXPANDED_DESKTOP:
                 return new ExpandedDesktopTile(this);
+            case QSConstants.TILE_SYNC:
+                return new SyncTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }
