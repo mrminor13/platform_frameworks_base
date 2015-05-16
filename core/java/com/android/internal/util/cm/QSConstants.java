122eb40a8abf043c873be45ce77d2dc17cb5618b
aow1980 <wilkes.allen@gmail.com>
2015-05-16 19:14:19 -0400

    SystemUI : Add screen timeout tile
    Change-Id: I0854e940f9604dee2fff5093e688ef3a17b258a9
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    (cherry picked from commit 806f52027cec280105b62b64932bc76011678da7)

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -39,6 +39,7 @@ public class QSConstants {
     public static final String TILE_BATTERY_SAVER = "battery_saver";
     public static final String TILE_EXPANDED_DESKTOP = "expanded_desktop";
     public static final String TILE_SYNC = "sync";
+    public static final String TILE_SCREEN_TIMEOUT = "screen_timeout";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -68,5 +69,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_BATTERY_SAVER);
         TILES_AVAILABLE.add(TILE_EXPANDED_DESKTOP);
         TILES_AVAILABLE.add(TILE_SYNC);
+	TILES_AVAILABLE.add(TILE_SCREEN_TIMEOUT);
     }
 }
