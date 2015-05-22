a2305179205f137fb565eaf1fd3ecdf1cfad1639
ezio84 <brabus84@gmail.com>
2015-05-22 12:32:22 +0200

    fix CoS immersive mode
    restoring https://review.slimroms.eu/#/c/4257/2
    
    patchset: define mContext
    
    Change-Id: I1ee1d5b5189a39707cf1e65259cfc4ee21999573

--- core/java/com/android/internal/util/slim/ImageHelper.java
+++ core/java/com/android/internal/util/slim/ImageHelper.java
@@ -59,6 +59,20 @@ public class ImageHelper {
         cc.drawBitmap(grayscaleBitmap, rect, rect, pp);
         return new BitmapDrawable(grayscaleBitmap);
     }
+ 
+    public static Bitmap drawableToBitmap (Drawable drawable) {
+        if (drawable == null) {
+            return null;
+        } else if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+                drawable.getIntrinsicHeight(), Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
     
     public static Bitmap getColoredBitmap(Drawable d, int color) {
         if (d == null) {
