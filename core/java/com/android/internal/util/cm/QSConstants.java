96a2b2c8ed9cc82b17dd63c85109457c02941c39
ezio84 <brabus84@gmail.com>
2015-05-30 15:19:10 +0200

    QS: add enable/disable LED tile [2/2]
    Change-Id: I7bb9a4854a753ec1256a918d827d8ed65da17862
    PS1: new vector icon thanks to Michele Amendolagine

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -43,6 +43,7 @@ public class QSConstants {
     public static final String TILE_SCREEN_OFF = "screen_off";
     public static final String TILE_USB_TETHER = "usb_tether";
     public static final String TILE_SCREENSHOT = "screenshot";
+    public static final String TILE_LED = "led";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -76,5 +77,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_SCREEN_OFF);
         TILES_AVAILABLE.add(TILE_USB_TETHER);
         TILES_AVAILABLE.add(TILE_SCREENSHOT);
+        TILES_AVAILABLE.add(TILE_LED);
     }
 }
