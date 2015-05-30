CyanogenMod/android_frameworks_base

livedisplay: Implement active display adjustment
 * LiveDisplay is our new name for the various display technologies
   which adjust the screen based on environmental conditions other
   than the standard automatic backlight controls.
 * This patch implements automatic color temperature adjustment based
   on time of day. This is similar to f.lux or Redshift. My eyes are
   so happy now!
 * Automatic outdoor/SRE feature is now handled here.
 * Handling of CABC/CABL and color enhancements also handled here.
 * Manual RGB tweaking is handled here.
 * Can delegate to DisplayColor HAL if available, otherwise uses
   SurfaceFlinger's 1015 operation to apply changes.
 * Happily coexists with the new accessibility features for color
   blindness correction and color inversion.

Change-Id: Iac1b74f410957f8e2d8290465c4ce9cc1fd97a88

packages/SystemUI/src/com/android/systemui/qs/tiles/LiveDisplayTile.java
@@ -1,0 +1,149 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import com.android.internal.util.ArrayUtils;
+import com.android.systemui.R;
+import com.android.systemui.qs.QSTile;
+
+/** Quick settings tile: LiveDisplay mode switcher **/
+public class LiveDisplayTile extends QSTile<LiveDisplayTile.LiveDisplayState> {
+    private static final Intent LIVEDISPLAY_SETTINGS = new Intent("android.settings.LIVEDISPLAY_SETTINGS");
+
+    private final LiveDisplayObserver mObserver;
+    private final String[] mEntries;
+    private final String[] mValues;
+    private final int[] mEntryIconRes;
+
+    private boolean mListening;
+
+    public LiveDisplayTile(Host host) {
+        super(host);
+
+        Resources res = mContext.getResources();
+        TypedArray typedArray = res.obtainTypedArray(R.array.live_display_drawables);
+        mEntryIconRes = new int[typedArray.length()];
+        for (int i = 0; i < mEntryIconRes.length; i++) {
+            mEntryIconRes[i] = typedArray.getResourceId(i, 0);
+        }
+        typedArray.recycle();
+
+        mEntries = res.getStringArray(com.android.internal.R.array.live_display_entries);
+        mValues = res.getStringArray(com.android.internal.R.array.live_display_values);
+
+        mObserver = new LiveDisplayObserver(mHandler);
+        mObserver.startObserving();
+    }
+
+    @Override
+    protected LiveDisplayState newTileState() {
+        return new LiveDisplayState();
+    }
+
+    @Override
+    public void setListening(boolean listening) {
+        if (mListening == listening)
+            return;
+        mListening = listening;
+        if (listening) {
+            mObserver.startObserving();
+        } else {
+            mObserver.endObserving();
+        }
+    }
+
+    @Override
+    protected void handleClick() {
+        changeToNextMode();
+    }
+
+    @Override
+    protected void handleLongClick() {
+        mHost.startSettingsActivity(LIVEDISPLAY_SETTINGS);
+    }
+
+    @Override
+    protected void handleUpdateState(LiveDisplayState state, Object arg) {
+        state.visible = true;
+        state.mode = arg == null ? getCurrentModeIndex() : (Integer) arg;
+        state.label = mEntries[state.mode];
+        state.icon = mContext.getDrawable(mEntryIconRes[state.mode]);
+    }
+
+    private int getCurrentModeIndex() {
+        return ArrayUtils.indexOf(mValues,
+                String.valueOf(Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.DISPLAY_TEMPERATURE_MODE,
+                        0, UserHandle.USER_CURRENT)));
+    }
+
+    private void changeToNextMode() {
+        int next = getCurrentModeIndex() + 1;
+        if (next >= mValues.length) {
+            next = 0;
+        }
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.DISPLAY_TEMPERATURE_MODE,
+                Integer.valueOf(mValues[next]), UserHandle.USER_CURRENT);
+    }
+
+    private class LiveDisplayObserver extends ContentObserver {
+        public LiveDisplayObserver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            refreshState(getCurrentModeIndex());
+        }
+
+        public void startObserving() {
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.DISPLAY_TEMPERATURE_MODE),
+                    false, this);
+        }
+
+        public void endObserving() {
+            mContext.getContentResolver().unregisterContentObserver(this);
+        }
+    }
+
+    public static class LiveDisplayState extends QSTile.State {
+        public int mode;
+
+        @Override
+        public boolean copyTo(State other) {
+            final LiveDisplayState o = (LiveDisplayState) other;
+            final boolean changed = mode != o.mode;
+            return super.copyTo(other) || changed;
+        }
+
+        @Override
+        protected StringBuilder toStringBuilder() {
+            final StringBuilder rt = super.toStringBuilder();
+            rt.insert(rt.length() - 1, ",mode=" + mode);
+            return rt;
+        }
+    }
+}

mrminor13/platform_frameworks_base

 * All postprocessing will be disabled when powersave mode is
   activated.
Conflicts:
	core/java/android/provider/Settings.java
	core/res/res/values/slim_strings.xml
	packages/SystemUI/res/values/slim_arrays.xml

packages/SystemUI/src/com/android/systemui/qs/tiles/LiveDisplayTile.java
@@ -1,0 +1,161 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.qs.tiles;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import com.android.internal.util.ArrayUtils;
+import com.android.systemui.R;
+import com.android.systemui.qs.QSTile;
+
+/** Quick settings tile: LiveDisplay mode switcher **/
+public class LiveDisplayTile extends QSTile<LiveDisplayTile.LiveDisplayState> {
+    private static final Intent LIVEDISPLAY_SETTINGS =
+            new Intent("android.settings.LIVEDISPLAY_SETTINGS");
+
+    private final LiveDisplayObserver mObserver;
+    private final String[] mEntries;
+    private final String[] mValues;
+    private final int[] mEntryIconRes;
+
+    private boolean mListening;
+
+    public static final int MODE_OUTDOOR = 3;
+
+    private final boolean mOutdoorModeAvailable;
+
+    public LiveDisplayTile(Host host) {
+        super(host);
+
+        Resources res = mContext.getResources();
+        TypedArray typedArray = res.obtainTypedArray(R.array.live_display_drawables);
+        mEntryIconRes = new int[typedArray.length()];
+        for (int i = 0; i < mEntryIconRes.length; i++) {
+            mEntryIconRes[i] = typedArray.getResourceId(i, 0);
+        }
+        typedArray.recycle();
+
+        mEntries = res.getStringArray(com.android.internal.R.array.live_display_entries);
+        mValues = res.getStringArray(com.android.internal.R.array.live_display_values);
+
+        mOutdoorModeAvailable = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.DISPLAY_AUTO_OUTDOOR_MODE,
+                -1, UserHandle.USER_CURRENT) > -1;
+
+        mObserver = new LiveDisplayObserver(mHandler);
+        mObserver.startObserving();
+    }
+
+    @Override
+    protected LiveDisplayState newTileState() {
+        return new LiveDisplayState();
+    }
+
+    @Override
+    public void setListening(boolean listening) {
+        if (mListening == listening)
+            return;
+        mListening = listening;
+        if (listening) {
+            mObserver.startObserving();
+        } else {
+            mObserver.endObserving();
+        }
+    }
+
+    @Override
+    protected void handleClick() {
+        changeToNextMode();
+    }
+
+    @Override
+    protected void handleLongClick() {
+        mHost.startSettingsActivity(LIVEDISPLAY_SETTINGS);
+    }
+
+    @Override
+    protected void handleUpdateState(LiveDisplayState state, Object arg) {
+        state.visible = true;
+        state.mode = arg == null ? getCurrentModeIndex() : (Integer) arg;
+        state.label = mEntries[state.mode];
+        state.icon = ResourceIcon.get(mEntryIconRes[state.mode]);
+    }
+
+    private int getCurrentModeIndex() {
+        return ArrayUtils.indexOf(mValues,
+                String.valueOf(Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.DISPLAY_TEMPERATURE_MODE,
+                        0, UserHandle.USER_CURRENT)));
+    }
+
+    private void changeToNextMode() {
+        int next = getCurrentModeIndex() + 1;
+        if (!mOutdoorModeAvailable && next == MODE_OUTDOOR) {
+            next++;
+        }
+        if (next >= mValues.length) {
+            next = 0;
+        }
+        Settings.System.putIntForUser(mContext.getContentResolver(),
+                Settings.System.DISPLAY_TEMPERATURE_MODE,
+                Integer.valueOf(mValues[next]), UserHandle.USER_CURRENT);
+    }
+
+    private class LiveDisplayObserver extends ContentObserver {
+        public LiveDisplayObserver(Handler handler) {
+            super(handler);
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            refreshState(getCurrentModeIndex());
+        }
+
+        public void startObserving() {
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.DISPLAY_TEMPERATURE_MODE),
+                    false, this);
+        }
+
+        public void endObserving() {
+            mContext.getContentResolver().unregisterContentObserver(this);
+        }
+    }
+
+    public static class LiveDisplayState extends QSTile.State {
+        public int mode;
+
+        @Override
+        public boolean copyTo(State other) {
+            final LiveDisplayState o = (LiveDisplayState) other;
+            final boolean changed = mode != o.mode;
+            return super.copyTo(other) || changed;
+        }
+
+        @Override
+        protected StringBuilder toStringBuilder() {
+            final StringBuilder rt = super.toStringBuilder();
+            rt.insert(rt.length() - 1, ",mode=" + mode);
+            return rt;
+        }
+    }
+}