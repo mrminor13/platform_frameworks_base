SlimRoms/frameworks_base

Allow disabling adjusting volume sounds [1/2]
Conflicts:
	core/java/android/provider/Settings.java

Change-Id: I3d5bdb85e3df508d256657355001ba4270354a79

core/java/android/provider/Settings.java
@@ -2453,6 +2453,12 @@ public final class Settings {
         public static final String SHOW_WEB_SUGGESTIONS = "show_web_suggestions";
 
         /**
+         * Whether the volume slider adjust sound is enabled. The value is
+         * boolean (1 or 0).
+         */
+        public static final String VOLUME_ADJUST_SOUND_ENABLED = "volume_adjust_sound_enabled";
+
+        /**
          * Whether the notification LED should repeatedly flash when a notification is
          * pending. The value is boolean (1 or 0).
          * @hide
@@ -2688,6 +2694,7 @@ public final class Settings {
             TTY_MODE,
             SOUND_EFFECTS_ENABLED,
             HAPTIC_FEEDBACK_ENABLED,
+            VOLUME_ADJUST_SOUND_ENABLED,
             POWER_SOUNDS_ENABLED,       // moved to global
             DOCK_SOUNDS_ENABLED,        // moved to global
             LOCKSCREEN_SOUNDS_ENABLED,
@@ -2710,6 +2717,7 @@ public final class Settings {
         public static final String[] CLONE_TO_MANAGED_PROFILE = {
             DATE_FORMAT,
             HAPTIC_FEEDBACK_ENABLED,
+            VOLUME_ADJUST_SOUND_ENABLED,
             SOUND_EFFECTS_ENABLED,
             TEXT_SHOW_PASSWORD,
             TIME_12_24

mrminor13/platform_frameworks_base

Change-Id: I2d167c2f01d20b128010197a05426618ae8b01b2

core/java/android/provider/Settings.java
@@ -2637,6 +2637,12 @@ public final class Settings {
         public static final String VOLUME_ADJUST_SOUND = "volume_adjust_sound";
 
         /**
+         * Whether the volume slider adjust sound is enabled. The value is
+         * boolean (1 or 0).
+         */
+        public static final String VOLUME_ADJUST_SOUND = "volume_adjust_sound";
+
+        /**
          * Whether the notification LED should repeatedly flash when a notification is
          * pending. The value is boolean (1 or 0).
          * @hide