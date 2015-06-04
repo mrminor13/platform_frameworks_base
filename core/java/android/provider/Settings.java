7e5040aa18e2ba0519e738bc421b6781401967e4
GWX <kagwind@gmail.com>
2015-06-04 11:43:30 +0800

    [1/2] Display cursor buttons in navigation bar while typing.
    Adapted for marshmallow
    Made new smaller vector icons
    
    -----
    
    Add ContentObserver for IME arrows.
    
    Change-Id: I071977c64fde6b0355367db7691f1b5ea17d9c44
    
    Fix IME Navigation up/down.
    
    Change-Id: I92b7990fd173b2adf34cda7091e288302da92b09
    
    Fix alphas of IME arrow buttons.
    
    Change-Id: I692003902a76f3e35104220907f965bf4234814c
    
    Fix ContentObserver for IME arrows.
    
    Change-Id: I8b04f67f5beabcccfb89a967a82286bddc7d5394
    
    Fix Menu button visibility issues.
    
    Fix Menu button shown in lockscreen.
    
    Change-Id: I639a21fc1f0fbaafb65bf46ed69a63ba0b32735f
    
    Fix menu button not reshown after unlock.
    
    Change-Id: I8bbeb8cac974d449806554dc1514e917368b5e94
    
    Set Menu buttons visibility inside setDisabledFlags function.
    
    Change-Id: I2e58703e6706a3727e91cae2f712b6b43ee34b7e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3705,6 +3705,12 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_FONT_STYLE = "statusbar_clock_font_style";
 
         /**
+         * Whether to show the IME arrows in the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_IME_ARROWS = "status_bar_ime_arrows";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
