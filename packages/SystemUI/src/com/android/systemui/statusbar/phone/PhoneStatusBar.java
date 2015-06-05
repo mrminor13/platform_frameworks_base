3a8890302dd293b5b0146c1227ff292714042095
GWX <kagwind@gmail.com>
2015-06-05 12:41:55 +0800

    Remove unused condition check.
    Change-Id: I56c3ef27815a7b823da80b1e3698dea5a7ea5dbc
    
    (cherry picked from commit 5323f3ded1d0ad9e4669bfdf9a97c5f2b10df676)
    
    (cherry picked from commit 5323f3ded1d0ad9e4669bfdf9a97c5f2b10df676)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -394,7 +394,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
     // for disabling the status bar
     int mDisabled = 0;
-    boolean mDisableHomeLongpress;
 
     // tracking calls to View.setSystemUiVisibility()
     int mSystemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE;
@@ -1372,7 +1371,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         public boolean onTouch(View v, MotionEvent event) {
             switch(event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                if (!shouldDisableNavbarGestures() && !mDisableHomeLongpress) {
+                if (!shouldDisableNavbarGestures()) {
                     mHandler.removeCallbacks(mShowSearchPanel);
                     mHandler.postDelayed(mShowSearchPanel, mShowSearchHoldoff);
                 }
