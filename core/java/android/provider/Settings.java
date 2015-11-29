b1cf16fa5771ab2af57e7fe573a80ba23f0d1971
Alex Cruz <mazdarider23@gmail.com>
2015-11-29 00:23:08 +0000

    Full screen recents [1/2]
    I got the idea from a post made in the PureNexus community
    https://plus.google.com/114489508152166298231/posts/CmRT9maDqMo
    
    - Turned off by default since this is not everyone's cup of tea
    
    Change-Id: I60d332908c75bf577be51ed891d88b4b91597cf2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3914,6 +3914,12 @@ public final class Settings {
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
+         * Whether to display recents in full screen
+         * @hide
+         */
+        public static final String RECENTS_FULL_SCREEN = "recents_full_screen";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
