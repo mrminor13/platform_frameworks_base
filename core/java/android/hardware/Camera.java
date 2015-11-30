82f666f4227f84bc861a4cf0103850b82c518e9b
Aldrin Holmes (CallMeAldy) <aldrin.holmes20@gmail.com>
2015-11-30 08:39:25 +0100

    Revert "Fix Photosphere/Camera FCs"
    This reverts commit 8bdc672ebb2b8d10a277eca65977bfbab66dd067.

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -3388,7 +3388,6 @@ public class Camera {
          * @see #getSceneMode()
          */
         public void setSceneMode(String value) {
-            if(getSupportedSceneModes() == null) return;
             set(KEY_SCENE_MODE, value);
         }
 
@@ -3426,7 +3425,6 @@ public class Camera {
          * @see #getFlashMode()
          */
         public void setFlashMode(String value) {
-	    if(getSupportedFlashModes() == null) return;
             set(KEY_FLASH_MODE, value);
         }
 
