60de73d8a03e66abcbc293fb0a2f4322d9a81752
Mark Lu <Mark_Lu@htc.com>
2016-03-23 19:08:44 +0800

    [AM] Skip unnessary ANR when process already died.
    If app process been killed by AM or lowmemkiller just before ANR report,
    ProcessRecord info will be cleared after received DeathRecipient,
    it also cannot dump trace log because process already dead,
    so report ANR & show ANR UI to let user wait seems is unnecessary.
    (compare normal ANR case, if kill app process by command,
    ANR dialog will also dismissed, it seems reasonable.)
    
    To check above condition, if ANR process pid is zero,
    it means process already dead & can skip report this ANR.
    
    Change-Id: I483cb02bacb10c32db80ca1097310b02abbac24d

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -4921,6 +4921,9 @@ public final class ActivityManagerService extends ActivityManagerNative
             } else if (app.crashing) {
                 Slog.i(TAG, "Crashing app skipping ANR: " + app + " " + annotation);
                 return;
+            } else if (app.pid == 0) {
+                Slog.i(TAG, "Skipping died app ANR: " + app + " " + annotation);
+                return;
             }
 
             // In case we come through here for the same app before completing
